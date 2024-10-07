2ad5177757cabb2730b1dc85fb20dc69a805f157
Shriya Pandey <pandeyshriya10@gmail.com>
2024-10-08 00:12:20 +0530

    updated

--- backend/controllers/tourControllers.js
+++ backend/controllers/tourControllers.js
@@ -14,3 +14,46 @@ export const createTour=async(req,res)=>{
     .json({success:false, message:'failed to create.Try again'})
   }
 }
+
+
+//update tour 
+export const updateTour=async(req,res)=>{
+  const id = req.prams.id
+  try {
+    const updateTour=await Tour. findByIdAndUpdate(id,{
+      $set: req.body
+    },{new:true})
+
+    res.status(200).json({success:true, message:'successfully updated', data:updateTour})
+  }
+   catch (err) {
+    res.status(500).json({success:false, message:'failed to update'})
+  }
+}
+
+//delete tour 
+export const deleteTour=async(req,res)=>{
+  try {
+    
+  } catch (err) {
+    
+  }
+}
+
+//getSingle tour 
+export const getSingleTour=async(req,res)=>{
+  try {
+    
+  } catch (err) {
+    
+  }
+}
+
+//getAll tour 
+export const getAllTour=async(req,res)=>{
+  try {
+    
+  } catch (err) {
+    
+  }
+}
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,8 +1,19 @@
 import express from 'express'
-import { createTour } from '../controllers/tourControllers.js';
+import { createTour, deleteTour, getAllTour, getSingleTour, updateTour } from '../controllers/tourControllers.js';
 const router= express.Router()
 
 //create new tour
-router.post('/',createTour)
+router.post('/',createTour);
 
+//update new tour
+router.put('/:id',updateTour);
+
+//delete new tour
+router.delete('/:id',deleteTour);
+
+//create new tour
+router.get('/:id',getSingleTour);
+
+//create new tour
+router.get('/:id',getAllTour);
 export default router;
